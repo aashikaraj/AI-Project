c9aea43c5d30a2953673e582baa7fb94d4ff7df7
Aashika Raj <126551058+aashikaraj@users.noreply.github.com>
2024-07-24 07:41:50 +0000

    navbar fixed

--- OJT/app.py
+++ OJT/app.py
@@ -1,54 +1,57 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+def get_current_page():
+    return request.endpoint
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', current_page=get_current_page())
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', current_page=get_current_page())
 
 @app.route('/booking')
 def booking():
-    return render_template('booking.html')
+    return render_template('booking.html', current_page=get_current_page())
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', current_page=get_current_page())
 
 @app.route('/destination')
 def destination():
-    return render_template('destination.html')
+    return render_template('destination.html', current_page=get_current_page())
 
 @app.route('/gallery')
 def gallery():
-    return render_template('gallery.html')
+    return render_template('gallery.html', current_page=get_current_page())
 
 @app.route('/guides')
 def guides():
-    return render_template('guides.html')
+    return render_template('guides.html', current_page=get_current_page())
 
 @app.route('/packages')
 def packages():
-    return render_template('packages.html')
+    return render_template('packages.html', current_page=get_current_page())
 
 @app.route('/services')
 def services():
-    return render_template('services.html')
+    return render_template('services.html', current_page=get_current_page())
 
 @app.route('/success')
 def success():
-    return render_template('success.html')
+    return render_template('success.html', current_page=get_current_page())
 
 @app.route('/testimonial')
 def testimonial():
-    return render_template('testimonial.html')
+    return render_template('testimonial.html', current_page=get_current_page())
 
 @app.route('/tour')
 def tour():
-    return render_template('tour.html')
+    return render_template('tour.html', current_page=get_current_page())
 
 if __name__ == '__main__':
     app.run(debug=True)
